d03559faf9e0652c0ace198ea38e094b4b6b9caf
Darryl <62851935+mr-subroutine@users.noreply.github.com>
2021-02-23 19:26:23 -0800

    Player Input Added
    The user is now able to move the paddle.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private float _speed = 0.5f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,24 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+
+    void Move()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        Vector3 direction = new Vector3(horizontalInput, 0, 0);
+        transform.Translate(direction * _speed * Time.deltaTime);
+        Debug.Log(transform.position);
+
+        if (transform.position.x < -3.1f)
+        {
+            transform.position = new Vector3(-3.1f, transform.position.y, 0);
+        }
+
+        if (transform.position.x > 3.1f)
+        {
+            transform.position = new Vector3(3.1f, transform.position.y, 0);
+        }
     }
 }
